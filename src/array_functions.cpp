76e6944b5d28156c9fee20b9dc852c0b6f68a94e
Guest <Gabriel.caicedo.17@cnu.edu>
2020-02-13 12:05:30 -0500

    First commit

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -7,18 +7,85 @@
 
 //============================================================================
 //	TODO add necessary includes here
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "array_functions.h"
+#include "constants.h"
+typedef std::string string;
+
 //============================================================================
 
 //============================================================================
 //	stuff you will need
 //============================================================================
 //TODO define a structure to track words and number of times they occur
+typedef struct wordCnt{
+	string word;
+	int number_occurence;
+}wordStruct;
 
 //TODO add a global array of entry structs (global to this file)
+wordStruct wordArray[];
 
 //TODO add variable to keep track of next available slot in array
 
 //TODO define all functions in header file
+//zero out array that tracks words and their occurrences
+void clearArray(){
+	for(int i = 0; i<wordArray.getArraySize(); i++){
+	wordArray[]
+	}
+
+}
+
+//how many unique words are in array
+int getArraySize(){
+	return number_occurence;
+}
+
+//get data at a particular location
+std::string getArrayWordAt(int i);
+int getArrayWord_NumbOccur_At(int i);
+
+/*loop through whole file, one line at a time
+ * call processLine on each line
+ * returns false: myfstream is not open
+ *         true: otherwise*/
+bool processFile(std::fstream &myfstream);
+
+/*take 1 line and extract all the tokens from it
+feed each token to processToken for recording*/
+void processLine(std::string &myString);
+
+/*Keep track of how many times each token seen*/
+void processToken(std::string &token);
+
+/*if you are debugging the file must be in the project parent directory
+  in this case Project2 with the .project and .cProject files*/
+bool openFile(std::fstream& myfile, const std::string& myFileName,
+		std::ios_base::openmode mode = std::ios_base::in);
+
+/*iff myfile is open then close it*/
+void closeFile(std::fstream& myfile);
+
+/* serializes all content in myEntryArray to file outputfilename
+ * returns  FAIL_FILE_DID_NOT_OPEN if cannot open outputfilename
+ * 			FAIL_NO_ARRAY_DATA if there are 0 entries in myEntryArray
+ * 			SUCCESS if all data is written and outputfilename closes OK
+ * */
+int writeArraytoFile(const std::string &outputfilename);
+
+/*
+ * Sort myEntryArray based on so enum value.
+ * The presence of the enum implies a switch statement
+ */
+void sortArray(constants::sortOrder so);
+
+#endif /* ARRAY_FUNCTIONS_H_ */
+
+
 
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
 
